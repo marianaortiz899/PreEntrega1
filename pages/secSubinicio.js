baca2907224efad6180ac1c78e03531925da0146
Mariana Ortiz <ortizmariana899@gmail.com>
2023-03-07 23:03:43 -0300

    add elements

--- pages/secSubinicio.js
+++ pages/secSubinicio.js
@@ -1,18 +1,1 @@
 
-const producto1 = 
-    [{ 
-            id: 1, 
-            producto: "San Rafael",
-            precio: "20000",
-            Numdepersonas: "2",
-            imagen: "../img/banner/SanRafael.png"},
-        {
-            id:3,
-            producto: "Malargue",
-            precio: "45000",
-            Numdepersonas: "4",
-            imagen: "../img/banner/Malargue.png",
-        },
-    ];
-producto1.forEach((producto1))
-const carrito = document.getElementById("")
